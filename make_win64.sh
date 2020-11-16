c07b29c05493e2ccef480d7ed5de7a58267b5723
sidav <sidav94@gmail.com>
2020-11-16 07:12:01 +0300

    Adds building script.

---
+++ make_win64.sh
@@ -0,0 +1,10 @@
+rm build_win64/*.exe
+cp -rf assets/ build_win64/
+
+build_datetime=`date '+%Y_%m_%d__%H_%M_%S'`;
+exename="TaffeRL_"$build_datetime".exe"
+
+echo "Building "$exename"..."
+
+env CGO_ENABLED="1" CC="/usr/bin/x86_64-w64-mingw32-gcc" GOOS="windows" CGO_LDFLAGS="-lmingw32 -lSDL2" CGO_CFLAGS="-D_REENTRANT" go build -o build_win64/$exename *.go
+
